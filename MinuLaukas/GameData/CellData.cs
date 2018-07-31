7e108ffc5e10c1143060aafdb208a943dbe43415
DESKTOP-OPCENTO\Jurate <jurate.valatkeviciene@gmail.com>
2018-07-31 22:23:30 +0300

    no message

---
+++ MinuLaukas/GameData/CellData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinuLaukas.GameData
+{
+    public enum CellState { tuscia, mina, skaicius }
+
+    public struct CellData
+    {
+        
+        public bool mode; // atidengta - 1 /uždengta - 0//bool
+        public CellState value; // tuscia; mina; skaicius; 
+        public char number;  // O 1 2 3 4 5 6 7 8 # - matomas simbolis
+        
+
+        public CellData(bool atidengta = false, CellState reiksme = CellState.tuscia, char num = 'O')
+        {
+            
+            mode = atidengta;
+            value = reiksme;
+            number = num;
+        }
+
+    }
+}
---
+++ MinuLaukas/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using MinuLaukas.Game;
+using MinuLaukas.MyMeniuWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinuLaukas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            MyMeniu Ekranas = new MyMeniu ();
+
+            Ekranas.PrintWindow();
+
+            GameRender Zaidimas = new GameRender();
+            
+            if (Ekranas.Valdymas() == 1)
+            {
+                Zaidimas.PrintZaidimas();
+            }
+
+           
+           // Console.ReadKey();
+        }
+    }
+}
